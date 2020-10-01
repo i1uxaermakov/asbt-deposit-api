f502da3dc14adc855805e40dcff3395b36743f4b
i1uxaermakov <ilya.ermakov.acc@gmail.com>
2020-10-01 03:13:04 -0500

    Initial Commit - Blank Project

---
+++ src/main/java/uz/asbt/asbtdepositapi/AsbtDepositApiApplication.java
@@ -0,0 +1,13 @@
+package uz.asbt.asbtdepositapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsbtDepositApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsbtDepositApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/asbt/asbtdepositapi/AsbtDepositApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.asbt.asbtdepositapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsbtDepositApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
